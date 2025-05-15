3b7959015519f4f3fdcbb79d141df13805c7fee2
itsuki794 <god_hand@yahoo.ne.jp>
2025-05-15 15:41:28 +0900

    動くかわからないけど変更しました。

--- sketch_may13a.ino
+++ sketch_may13a.ino
@@ -4,19 +4,20 @@
 
 CRGB leds[NUM];
 
-int count =0;
+int count = 0;
 void setup() {
-  FastLED.addLeds<NEOPIXEL, 6>(leds, NUM);
+  FastLED.addLeds<WS2812, 6>(leds, NUM);
   // put your setup code here, to run once:
 }
 
 void loop() {
+  count = 0;
   // put your main code here, to run repeatedly:
   for (int i = 0; i <= 9; i++) {
     leds[i].r = 255-count;
     leds[i].g = 0;
     leds[i].b = count;
-    count=count+20;
+    count = count+20;
   }
 
 
